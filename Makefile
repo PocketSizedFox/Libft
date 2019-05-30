91b00fc808b86f20a850ebee9625fb318f1faadf
Kaelin LEES <klees@c3r6s1.wethinkcode.co.za>
2019-05-30 09:40:54 +0200

    libft

--- Makefile
+++ Makefile
@@ -1,8 +1,7 @@
 NAME = libft.a
 
 SRC = ft_atoi.c ft_putchar.c ft_memset.c ft_strcat.c ft_strcmp.c ft_strcpy.c ft_strncat.c ft_bzero.c ft_memcpy.c ft_memccpy.c \
-	  ft_strlen.c ft_strdup.c ft_memmove.c ft_memchr.c ft_memcmp.c ft_strncpy.c ft_strncat.c ft_strlcat.c ft_strchr.c ft_strrchr.c ft_strstr.c ft_strnstr.c ft_strncmp.c \
-
+	  ft_strlen.c ft_strdup.c ft_memmove.c ft_memchr.c ft_memcmp.c ft_strncpy.c ft_strncat.c ft_strlcat.c ft_strchr.c ft_strrchr.c ft_strstr.c ft_strnstr.c ft_strncmp.c ft_isdigit.c ft_isalpha.c ft_isalnum.c ft_isascii.c ft_isprint.c ft_toupper.c ft_tolower.c \
 
 all :
 		gcc -c $(SRC) -Wextra -Werror -Wall -I .
--- libft.h
+++ libft.h
@@ -5,22 +5,20 @@
 #include <unistd.h>
   int	ft_strcmp(const char *s1, const char *s2);
   char	*ft_strcpy(char *dst, const char *src);
-  int ft_atoi(const char *str);
-  int ft_isupper(char *str);
-  void ft_putchar(char c);
-  int ft_isalpha(char c);
-  int ft_islower(char c);
-  int ft_isdigit(int c);
-  int ft_isprint(char c);
-  int ft_toupper(int c);
-  int ft_strlen(const char *str);
-  void ft_putnbr(int nb);
-  void *ft_memalloc(size_t size);
-  void *ft_memset(void *b, int c, size_t len);
-  char *ft_strdup(char *src);
-  char *ft_strcat(char *s1, const char *s2);
-  char *ft_strncat(char *s1, const char *s2, size_t n);
-  void ft_bzero(void *s, size_t n);
+  int	ft_atoi(const char *str);
+  int	ft_isupper(char *str);
+  void	ft_putchar(char c);
+  int	ft_islower(char c);
+  int	ft_isdigit(int c);
+  int	ft_toupper(int c);
+  int	ft_strlen(const char *str);
+  void	ft_putnbr(int nb);
+  void	*ft_memalloc(size_t size);
+  void	*ft_memset(void *b, int c, size_t len);
+  char	*ft_strdup(char *src);
+  char	*ft_strcat(char *s1, const char *s2);
+  char	*ft_strncat(char *s1, const char *s2, size_t n);
+  void	ft_bzero(void *s, size_t n);
   void	*ft_memcpy(void *restrict str1, const void *restrict str2, size_t n);
   void	*ft_memccpy(void *dest, const void *src, int c, size_t size);
   void	*ft_memmove(void *dst, const void *src, size_t len);
@@ -30,8 +28,10 @@
   size_t	ft_strlcat(char *dst, const char *src, size_t size);
   char	*ft_strchr(const char *s, int c);
   char	*ft_strrchr(const char *s, int c);
-  char *ft_strstr(char *str, char *to_find);
+  char	*ft_strstr(char *str, char *to_find);
   char	*ft_strnstr(const char *s1, const char *s2, size_t n);
+  int	ft_isalpha(int c);
   int	ft_strncmp(const char *s1, const char *s2, size_t n);
+  int	ft_isprint(int c);
 #endif /* _LIBFT_H_ */
 
